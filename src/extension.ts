40e3deef550675ef2b1225267c625cd39c848526
shanalikhan1 <shanalikhan@hotmail.com>
2016-03-10 21:34:23 +0500

    file manager with promises for interacting with FS

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,8 @@ import * as vscode from 'vscode';
 import * as pluginService from './pluginService'
 
 import * as path from 'path';
-import * as envi from './environmentPath';
+import * as envir from './environmentPath';
+import * as fileManager from './fileManager';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
     
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    var en : envi.Environment = new envi.Environment();
+    var en : envir.Environment = new envir.Environment();
     
     var openurl = require('open');
     var fs = require('fs');
---
+++ src/fileManager.ts
@@ -0,0 +1,56 @@
+
+export class FileManager {
+    private fs = null;
+    public FileManager() {
+        this.fs = require('fs');
+    }
+    public FileExists(filePath: string): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            var stat: boolean = this.fs.existsSync(filePath);
+            if (stat) {
+                resolve(stat);
+            }
+            else reject(stat);
+
+        });
+    }
+
+    public ReadFile(filePath: string): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            this.fs.readFile(filePath, { encoding: 'utf8' }, function(err: any, data: any) {
+                if (err) {
+                    //vscode.window.showErrorMessage(ERROR_MESSAGE);
+                    console.error(err);
+                    reject(null);
+                }
+                resolve(data);
+
+            });
+        });
+    }
+    public WriteFile(filePath : string , data : string) : Promise<boolean>{
+         return new Promise<boolean>((resolve, reject) => {
+                if (data) {
+                    
+                    this.fs.writeFile(filePath, data, function(err: any, data: any) {
+                        if (err) {
+                            //vscode.window.showErrorMessage(ERROR_MESSAGE);
+                            console.error(err);
+                            reject(false);
+                        }
+                        else {
+                            //TOKEN = token;
+                            resolve(true);
+                        }
+
+                    });
+                }
+                else {
+                    //vscode.window.showErrorMessage(ERROR_MESSAGE);
+                    console.error("DATA is EMPTY for "+ filePath);
+                    reject(false);
+                }
+
+            });
+    }
+}
