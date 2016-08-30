ea259a4f9e4427c982855996840c1e269d87c0c8
Shan Khan <Shan Khan>
2016-08-31 02:30:37 +0500

    first release ready for 2.2

--- src/environmentPath.ts
+++ src/environmentPath.ts
@@ -5,7 +5,7 @@ import {OsType} from './enums';
 
 export class Environment {
 
-    public static CURRENT_VERSION : number = 214;
+    public static CURRENT_VERSION : number = 220;
 
     private context: vscode.ExtensionContext;
     public isInsiders = null;
