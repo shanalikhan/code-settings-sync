6f67f48a9ae958eb462dfac15abdb87f3efbbf17
shanalikhan1 <shanalikhan@hotmail.com>
2016-08-31 15:34:13 +0500

    new version for allow upload check removed

--- src/environmentPath.ts
+++ src/environmentPath.ts
@@ -5,7 +5,7 @@ import {OsType} from './enums';
 
 export class Environment {
 
-    public static CURRENT_VERSION : number = 221;
+    public static CURRENT_VERSION : number = 222;
 
     private context: vscode.ExtensionContext;
     public isInsiders = null;
