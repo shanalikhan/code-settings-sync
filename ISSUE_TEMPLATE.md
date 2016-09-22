4c6ce8154e16f2d12cc89549dcb30a4c39d52515
shanalikhan1 <shanalikhan@hotmail.com>
2016-09-22 21:49:25 +0500

    settings changed and issue template changed

--- ISSUE_TEMPLATE.md
+++ ISSUE_TEMPLATE.md
@@ -1,5 +1,6 @@
 **Visual Studio Code Version :** [ VERSION_HERE ]
 **Code Sync Settings Version :** [ VERSION_HERE ]
 **Operating System :** [ VERSION_HERE ]
+**Occurs On:** [Upload / Download]
 
 Please make sure to write the versions and console logged error picture or copied text.
--- src/setting.ts
+++ src/setting.ts
@@ -1,30 +1,31 @@
 import {Environment} from './environmentPath';
 
 "use strict";
-export class LocalSetting{
-    public Token : string = null;
-    public Gist : string = null;
-    public lastUpload : Date = null;       
-    public firstTime : boolean = true;  // to open the toturial first time when used any command.
-    public autoDownload : boolean = false;
-    public lastDownload : Date = null;
-    public Version : number = null;
-    public showSummary : boolean = true;
-    public allowUpload : boolean = true;
+export class LocalSetting {
+    public Token: string = null;
+    public Gist: string = null;
+    public lastUpload: Date = null;
+    public firstTime: boolean = true;  // to open the toturial first time when used any command.
+    public autoDownload: boolean = false;
+    public autoUpload = false;
+    public allowUpload: boolean = true;
+    public lastDownload: Date = null;
+    public Version: number = null;
+    public showSummary: boolean = true;
     public publicGist = false;
     public forceDownload = false;
     public showLog = false;
-    public autoUpload = true;
+
 }
 
-export class CloudSetting{
-    public lastUpload : Date = null;
+export class CloudSetting {
+    public lastUpload: Date = null;
 }
 
-export class OldSetting{
-     public Token : string = null;
-    public Gist : string = null;
-    public Migrated:boolean  = true;
-    public ProxyIP :string = null;
-    public ProxyPort:string = null;
+export class OldSetting {
+    public Token: string = null;
+    public Gist: string = null;
+    public Migrated: boolean = true;
+    public ProxyIP: string = null;
+    public ProxyPort: string = null;
 }
