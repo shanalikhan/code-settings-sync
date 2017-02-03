f66bd851d128caee9de8a92caba9ec1c763118e5
Alexander Groß <agross@therightstuff.de>
2017-02-03 18:57:22 +0100

    Assign Gist date and actually use it for the up-to-date check, closes #206

--- src/extension.ts
+++ src/extension.ts
@@ -297,24 +297,22 @@ export async function activate(context: vscode.ExtensionContext) {
                     var keys = Object.keys(res.files);
                     if (keys.indexOf(en.FILE_CLOUDSETTINGS_NAME) > -1) {
                         var cloudSettGist: Object = JSON.parse(res.files[en.FILE_CLOUDSETTINGS_NAME].content);
-                        
-                        var cloudSett : CloudSetting = new CloudSetting();
-                        cloudSett.lastUpload = new Date(cloudSett.lastUpload);
+                        var cloudSett : CloudSetting = Object.assign(new CloudSetting(), cloudSettGist);;
 
                         let lastUploadStr: string = syncSetting.lastUpload.toString();
                         let lastDownloadStr: string = syncSetting.lastDownload.toString();
 
-                        var stat: boolean = false;
+                        var upToDate: boolean = false;
                         if (lastDownloadStr != "") {
-                            stat = new Date(syncSetting.lastDownload).getTime() === new Date(cloudSett.lastUpload).getTime();
+                            upToDate = new Date(lastDownloadStr).getTime() === new Date(cloudSett.lastUpload).getTime();
                         }
 
                         if (lastUploadStr != "") {
-                            stat = stat || new Date(syncSetting.lastUpload).getTime() === new Date(cloudSett.lastUpload).getTime();
+                            upToDate = upToDate || new Date(lastUploadStr).getTime() === new Date(cloudSett.lastUpload).getTime();
                         }
 
                         if (!syncSetting.forceDownload) {
-                            if (stat) {
+                            if (upToDate) {
                                 vscode.window.setStatusBarMessage("");
                                 vscode.window.setStatusBarMessage("Sync : You already have latest version of saved settings.", 5000);
                                 return;
