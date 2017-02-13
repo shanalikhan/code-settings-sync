999ee7f8964c2833dfc6eccfde8de00757c4cb04
Shan Khan <shan.ali@nakisa.com>
2017-02-13 21:33:31 +0500

    merged fixed

--- src/environmentPath.ts
+++ src/environmentPath.ts
@@ -5,11 +5,7 @@ import { OsType } from './enums';
 
 export class Environment {
 
-<<<<<<< HEAD
     public static CURRENT_VERSION: number = 244;
-=======
-    public static CURRENT_VERSION: number = 243;
->>>>>>> d4df114045c51058dbda5e0e1f80bae6efdb9ed1
     public static getVersion(): string {
         var txt2 =  Environment.CURRENT_VERSION.toString().slice(0, 1) + "." +  Environment.CURRENT_VERSION.toString().slice(1,2)+ "." +  Environment.CURRENT_VERSION.toString().slice(2,3);
         return txt2;
