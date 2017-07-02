56601e7ad9e05fb91f0f9df2e4d19468ce7ab991
Shan <shanalikhan@hotmail.com>
2017-07-02 13:15:28 +0500

    new version release

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+#### Version - 2.8.2
+
+1. Adding more checks for People getting [issue](https://github.com/shanalikhan/code-settings-sync/issues/343) like "Not Found". I am adding new option to store settings in onedrive and retrive from it for people having problems connecting GitHub.
+
+2. Fixing [issue](https://github.com/shanalikhan/code-settings-sync/issues/342)
+
+
+For Previous releases change log view the [post](http://shanalikhan.github.io/2016/05/14/Visual-studio-code-sync-settings-release-notes.html)
--- src/commons.ts
+++ src/commons.ts
@@ -312,14 +312,14 @@ export class Commons {
                         vscode.window.showInformationMessage("Sync : Now You can set your GitHub token manually in `syncLocalSettings.json`");
                     }
                 }
-                  vscode.window.showInformationMessage("Sync : Updated v"+ Environment.getVersion(),"Release Notes","Write Review","Donate Now").then(function(val: string){
+                  vscode.window.showInformationMessage("Sync : Updated to v"+ Environment.getVersion(),"Release Notes","Write Review","Support This Project").then(function(val: string){
                     if(val == "Release Notes"){
                         openurl("http://shanalikhan.github.io/2016/05/14/Visual-studio-code-sync-settings-release-notes.html");
                     }
                     if(val=="Write Review"){
                         openurl("https://marketplace.visualstudio.com/items?itemName=Shan.code-settings-sync#review-details");
                     }
-                    if(val=="Donate Now"){
+                    if(val=="Support This Project"){
                         openurl("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=4W3EWHHBSYMM8&lc=IE&item_name=Code%20Settings%20Sync&item_number=visual%20studio%20code%20settings%20sync&currency_code=USD&bn=PP-DonationsBF:btn_donate_SM.gif:NonHosted");
                     }
                 });
