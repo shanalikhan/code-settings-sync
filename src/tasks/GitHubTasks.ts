742bbab6c1fd518d6914ec15d8cbda086640a59d
Shan <shanalikhan@hotmail.com>
2017-08-08 03:01:08 +0500

    working on losely coupled classes

--- src/tasks/GitHubTasks.ts
+++ src/tasks/GitHubTasks.ts
@@ -1,12 +1,30 @@
 import { ITask } from './ITask';
+import {GitHubService} from '../services/githubService'
+import * as vscode from 'vscode';
+import {Environment} from '../common/environmentPath'
+import {Commons} from '../common/commons'
+
 
 export class GitHubTasks implements ITask {
 
-    constructor(private TOKEN : string){
+    private gitService : GitHubService = null;
+    private en : Environment = null;
+    private com : Commons = null;
 
+    constructor(private TOKEN : string, private context :vscode.ExtensionContext ){
+        this.gitService = new GitHubService(TOKEN);
+        this.en = new Environment(this.context);
+        this.com = new Commons(this.en,this.context);
     }
     Create(): Promise<string> {
-        throw new Error("Method not implemented.");
+        var self = this;
+        return new Promise<string>((resolve,reject)=>{
+            if(self.TOKEN==""){
+                reject("Sync : Set GitHub Token or set anonymousGist to true from settings.");
+            }
+            
+        });
+        //throw new Error("Method not implemented.");
     }
 
     Save(): Promise<boolean> {
