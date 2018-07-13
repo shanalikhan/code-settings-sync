b13f7de843737dde463bbec06f91a55560c72986
Axetroy <troy450409405@gmail.com>
2018-07-14 03:48:31 +0800

    feat: support i18n for extension. currently only supports English and Chinese. (#582)
    * Create CODE_OF_CONDUCT.md (#556)
    
    * Update issue templates
    
    * Delete ISSUE_TEMPLATE.md
    
    * Update issue templates
    
    * Update issue templates
    
    * feat: support i18n for extension configuration and command. only english and chinese
    
    * feat: translate extension.updateSetting command #581
    
    * refactor: localize accept rest params instead of an array
    
    * feat: translate extension.downloadSettings command #582
    
    * feat: translate extesion.resetSettings command #582
    
    * feat: translate extension.otherOptions command #582
    
    * feat: translate common message and remove conosle message, console message is only for development.
    
    * fix: invalid url string for HowSetting command. or it will throw error '[UriError]: Scheme contains illegal characters.'
    
    * refactor: Optimize translations
    
    * feat: make sure always got a valid localize config

---
+++ src/localize.ts
@@ -0,0 +1,99 @@
+import * as fs from "fs";
+import * as path from "path";
+
+interface IConfig {
+  locale?: string;
+}
+
+interface ILanguagePack {
+  [key: string]: string;
+}
+
+export class Localize {
+  // get language pack when the instance be created
+  private bundle = this.resolveLanguagePack();
+  constructor(private config: IConfig = {}) {}
+  /**
+   * translate the key
+   * @param key
+   * @param args
+   */
+  public localize(key: string, ...args: any[]) {
+    const languagePack = this.bundle;
+    const message: string = languagePack[key] || key;
+    return this.format(message, args);
+  }
+  /**
+   * format the message
+   * @param message
+   * @param args
+   */
+  private format(message: string, args: any[] = []): string {
+    let result: string;
+    if (args.length === 0) {
+      result = message;
+    } else {
+      result = message.replace(/\{(\d+)\}/g, (match, rest: any[]) => {
+        const index = rest[0];
+        return typeof args[index] !== "undefined" ? args[index] : match;
+      });
+    }
+    return result;
+  }
+  /**
+   * Get language pack
+   */
+  private resolveLanguagePack(): ILanguagePack {
+    let resolvedLanguage: string = "";
+    // TODO: it should read the extension root path from context
+    const rootPath = path.join(__dirname, "..", "..");
+    const file = path.join(rootPath, "package");
+    const options = this.config;
+
+    if (!options.locale) {
+      resolvedLanguage = ".nls.json";
+    } else {
+      let locale: string | null = options.locale;
+      while (locale) {
+        const candidate = ".nls." + locale + ".json";
+        if (fs.existsSync(file + candidate)) {
+          resolvedLanguage = candidate;
+          break;
+        } else {
+          const index = locale.lastIndexOf("-");
+          if (index > 0) {
+            locale = locale.substring(0, index);
+          } else {
+            resolvedLanguage = ".nls.json";
+            locale = null;
+          }
+        }
+      }
+    }
+
+    const languageFilePath = path.join(file + resolvedLanguage);
+
+    if (!fs.existsSync(languageFilePath)) {
+      return {};
+    }
+
+    return require(languageFilePath);
+  }
+}
+
+let config: IConfig = {
+  locale: "en"
+};
+
+try {
+  config = Object.assign(
+    config,
+    JSON.parse((process.env as any).VSCODE_NLS_CONFIG)
+  );
+} catch (err) {
+  //
+}
+
+const instance = new Localize(config);
+
+export default instance.localize.bind(instance);
