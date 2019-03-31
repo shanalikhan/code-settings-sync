db13734443150c1b0c864abebe80ad22c66ec72f
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-03-30 22:57:53 -0700

    Move extConfig to here and organize neatly

--- src/setting.ts
+++ src/setting.ts
@@ -1,20 +1,10 @@
 "use strict";
 import { Environment } from "./environmentPath";
 
-export class ExtensionConfig {
-  public quietSync: boolean = false;
-  public removeExtensions: boolean = true;
-  public syncExtensions: boolean = true;
-  public autoDownload: boolean = false;
-  public autoUpload = false;
-  public forceDownload: boolean = false;
-}
-
 export class LocalConfig {
   public publicGist: boolean = false;
-  public userName: string = null;
-  public name: string = null;
-  public extConfig: ExtensionConfig = new ExtensionConfig();
+  public userName: string = "";
+  public name: string = "";
   public customConfig: CustomSettings = new CustomSettings();
 }
 
@@ -25,27 +15,13 @@ export class CloudSetting {
 
 export class CustomSettings {
   public repoSettings = {
-    ignoredItems: [
-      "syncLocalSettings",
-      "workspaceStorage",
-      "globalStorage/state.vscdb"
-    ],
     repo: "",
     token: "",
     username: ""
   };
   public gistSettings = {
     gist: "",
-    ignoreUploadFiles: [
-      "projects.json",
-      "projects_cache_vscode.json",
-      "projects_cache_git.json",
-      "projects_cache_svn.json",
-      "gpm_projects.json",
-      "gpm-recentItems.json"
-    ],
-    ignoreUploadFolders: ["workspaceStorage"],
-    customFiles: {},
+    customFiles: [],
     githubEnterpriseUrl: null,
     askGistName: false,
     downloadPublicGist: false,
@@ -57,9 +33,29 @@ export class CustomSettings {
     lastDownload: null,
     ignoreUploadSettings: []
   };
-  public ignoredExtensions: string[] = [];
+  public ignoredItems = [
+    ".git",
+    "syncLocalSettings.json",
+    "sync.lock",
+    "workspaceStorage",
+    "globalStorage/state.vscdb",
+    "globalStorage/state.vscdb.backup",
+    "projects.json",
+    "projects_cache_vscode.json",
+    "projects_cache_git.json",
+    "projects_cache_svn.json",
+    "gpm_projects.json",
+    "gpm-recentItems.json"
+  ];
   public version: number = Environment.CURRENT_VERSION;
-  public disableUpdateMessage: boolean = true;
-  public hostName: string = null;
+  public hostname: string = "";
+  public ignoredExtensions: string[] = [];
   public syncMethod: "repo" | "gist" = "gist";
+  public autoDownload: boolean = false;
+  public autoUpload: boolean = false;
+  public forceDownload: boolean = false;
+  public quietSync: boolean = true;
+  public removeExtensions: boolean = true;
+  public syncExtensions: boolean = true;
+  public disableUpdateMessage: boolean = true;
 }
