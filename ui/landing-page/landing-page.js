c45b869e1eae10698701e8ff441073be8a2e1c76
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-04-04 09:36:23 -0700

    WIP. Update to look like Bookmarks extension

--- ui/landing-page/landing-page.js
+++ ui/landing-page/landing-page.js
@@ -3,20 +3,3 @@ const vscode = acquireVsCodeApi();
 function logInWithGitHub() {
   vscode.postMessage({});
 }
-
-(() => {
-  let deg = 0;
-  let mouseover = false;
-
-  $(".animated-logo").hover(
-    () => (mouseover = true),
-    () => (mouseover = false)
-  );
-
-  setInterval(() => {
-    if (mouseover) {
-      $(".animated-logo").css("transform", `rotate(${deg}deg)`);
-      deg += 5;
-    }
-  }, 20);
-})();
