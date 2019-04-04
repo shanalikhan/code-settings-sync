cf2da3948e6649ea5245948ba20af1128f227bc3
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-04-04 10:48:57 -0700

    Update

--- ui/landing-page/landing-page.js
+++ ui/landing-page/landing-page.js
@@ -3,3 +3,12 @@ const vscode = acquireVsCodeApi();
 function logInWithGitHub() {
   vscode.postMessage({});
 }
+
+function appendHTML(parent, html) {
+  var div = document.createElement("div");
+  div.innerHTML = html;
+  while (div.children.length > 0) {
+    parent.appendChild(div.children[0]);
+  }
+  div.remove();
+}
