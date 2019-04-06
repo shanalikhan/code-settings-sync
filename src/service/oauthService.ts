4da5263e341935e51c42ad5706d56c5eaa92d27c
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-04-05 19:24:06 -0700

    Allow user to select the gist they want to use

--- src/service/oauthService.ts
+++ src/service/oauthService.ts
@@ -1,23 +1,35 @@
 import * as express from "express";
+import { readFileSync } from "fs";
 import { Server } from "http";
 import fetch from "node-fetch";
 import { URLSearchParams } from "url";
+import * as vscode from "vscode";
 import Commons from "../commons";
 
 export class GitHubOAuthService {
   public app: express.Express;
   public server: Server;
+  public GistSelectionView: string;
 
-  constructor(public port: number, public commons: Commons) {
+  constructor(
+    public port: number,
+    public commons: Commons,
+    public extensionPath: string
+  ) {
     this.app = express();
     this.app.use(express.json(), express.urlencoded({ extended: false }));
+    this.GistSelectionView = readFileSync(
+      `${this.extensionPath}/ui/gist-selection/gist-selection.html`,
+      { encoding: "utf8" }
+    );
   }
 
   public async StartProcess() {
     this.server = this.app.listen(this.port);
     this.app.get("/callback", async (req, res) => {
-      const tokenres = await this.getToken(req.param("code"));
-      const params = new URLSearchParams(await tokenres.text());
+      const params = new URLSearchParams(
+        await (await this.getToken(req.param("code"))).text()
+      );
 
       res.send("<script>window.close();</script>");
       this.server.close();
@@ -27,8 +39,8 @@ export class GitHubOAuthService {
 
       const user = await this.getUser(token);
 
-      const gistId = await this.getGistId(token, user);
-      this.saveGistId(gistId);
+      const gists = await this.getGists(token, user);
+      this.saveGistId(gists);
     });
   }
 
@@ -44,17 +56,13 @@ export class GitHubOAuthService {
     });
   }
 
-  public async getGistId(token: string, user: string) {
+  public async getGists(token: string, user: string) {
     const res = await fetch(`https://api.github.com/users/${user}/gists`, {
       method: "GET",
       headers: { Authorization: `token ${token}` }
     });
-    const json = await res.json();
-    const gistsWithCloudSettings = json.filter(
-      gist => gist.files.cloudSettings !== undefined
-    );
-    const gistId = gistsWithCloudSettings ? gistsWithCloudSettings[0].id : "";
-    return gistId;
+    const gists = await res.json();
+    return gists;
   }
 
   public async saveToken(token: string) {
@@ -63,10 +71,36 @@ export class GitHubOAuthService {
     this.commons.SetCustomSettings(currentSettings);
   }
 
-  public async saveGistId(gistId: string) {
-    const extSettings = await this.commons.GetSettings();
-    extSettings.gist = gistId;
-    this.commons.SaveSettings(extSettings);
+  public async saveGistId(gists: any) {
+    const content: string = this.GistSelectionView.replace(
+      new RegExp("@GISTS", "g"),
+      JSON.stringify(gists)
+    ).replace(
+      new RegExp("@PWD", "g"),
+      vscode.Uri.file(this.extensionPath)
+        .with({
+          scheme: "vscode-resource"
+        })
+        .toString()
+    );
+    const gistSelectionPanel = vscode.window.createWebviewPanel(
+      "syncSettings",
+      "Sync Settings",
+      vscode.ViewColumn.One,
+      {
+        retainContextWhenHidden: true,
+        enableScripts: true
+      }
+    );
+    gistSelectionPanel.webview.html = content;
+    gistSelectionPanel.webview.onDidReceiveMessage(async message => {
+      if (!message.close) {
+        const extSettings = await this.commons.GetSettings();
+        extSettings.gist = message.id;
+        this.commons.SaveSettings(extSettings);
+      }
+      return gistSelectionPanel.dispose();
+    });
   }
 
   public async getUser(token: string) {
