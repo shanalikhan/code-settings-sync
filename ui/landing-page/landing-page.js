349d46bb867c2841caadb3ff90d2782d0477af97
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-04-18 16:29:20 -0700

    improve(ReleaseNotes):
    release-notes.json
    ----
    Update release notes for v3.2.9
    Make adding author and PR easier
    
    landing-page.js
    ----
    Implement new method for showing author and PR

--- ui/landing-page/landing-page.js
+++ ui/landing-page/landing-page.js
@@ -16,14 +16,16 @@ function appendHTML(parent, html) {
   div.remove();
 }
 
-const releaseNoteTemplate = `<h5 class="change text-white-50 mx-auto mt-2 mb-2"><span class="badge badge-@COLOR mr-2">@TYPE</span>@NOTE</h5>`;
+const releaseNoteTemplate = `<h5 class="change text-white-50 mx-auto mt-2 mb-2"><span class="badge badge-@COLOR mr-2">@TYPE</span>@NOTE (Thanks to <a href='https://github.com/@AUTHOR'>@@AUTHOR</a> for PR <a href='https://github.com/shanalikhan/code-settings-sync/pull/@PR'>#@PR</a>)</h5>`;
 
 const notesElement = document.getElementById("notes");
 releaseNotes.changes.forEach(change => {
   const html = releaseNoteTemplate
     .replace(new RegExp("@NOTE", "g"), change.details)
     .replace(new RegExp("@TYPE", "g"), change.type)
-    .replace(new RegExp("@COLOR", "g"), change.color);
+    .replace(new RegExp("@COLOR", "g"), change.color)
+    .replace(new RegExp("@AUTHOR", "g"), change.author)
+    .replace(new RegExp("@PR", "g"), change.pullRequest);
   appendHTML(notesElement, html);
 });
 
