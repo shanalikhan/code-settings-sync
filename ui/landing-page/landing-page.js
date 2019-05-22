b4825ceb05adef37a6c9ca79ac3bdf72bb64a993
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-05-21 17:19:17 -0700

    Fix issues with themes

--- ui/landing-page/landing-page.js
+++ ui/landing-page/landing-page.js
@@ -16,7 +16,7 @@ function appendHTML(parent, html) {
   div.remove();
 }
 
-const releaseNoteTemplate = `<h5 class="change text-white-50 mx-auto mt-2 mb-2"><span class="badge badge-@COLOR mr-2">@TYPE</span>@NOTE (Thanks to <a href='https://github.com/@AUTHOR'>@@AUTHOR</a> for PR <a href='https://github.com/shanalikhan/code-settings-sync/pull/@PR'>#@PR</a>)</h5>`;
+const releaseNoteTemplate = `<h5 class="change text-white-50a mx-auto mt-2 mb-2"><span class="badge badge-@COLOR mr-2">@TYPE</span>@NOTE (Thanks to <a href='https://github.com/@AUTHOR'>@@AUTHOR</a> for PR <a href='https://github.com/shanalikhan/code-settings-sync/pull/@PR'>#@PR</a>)</h5>`;
 
 const notesElement = document.getElementById("notes");
 releaseNotes.changes.forEach(change => {
